06a836284c40c9d11f070eb62fb910a03b235d17
Wahybs <aziz-zribi@outlook.fr>
2025-01-10 10:25:59 +0100

    Challenge 1

---
+++ Challenge1/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"""
+    <html>
+        <head>
+            <title>My Flask App</title>
+        </head>
+        <body>
+            <h1>Aziz ZRIBI</h1>
+            <h2>Nom Projet: NET 4255 serveur flask</h2>
+            <h3>Version: V1</h3>
+            <h4>Server Hostname: {request.url}</h4>
+            <h5>Current Date: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}</h5>
+        </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
